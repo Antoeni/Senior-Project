7567929e081d8991802ee4fac046d79dcc0582ab
Antoeni <nguyenanthony021029@gmail.com>
2024-04-06 21:19:27 -0700

    added in new model and different video

--- objectCounter.py
+++ objectCounter.py
@@ -1,8 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8n.yaml')  # build a new model from YAML
 model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
-model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
 
 # Train the model
-results = model.train(data='coco128.yaml', epochs=100, imgsz=640)
+results = model.train(data='config.yaml', epochs=100, imgsz=640)
